5d13ffb70cfbfbd6426690fa204086b276916be9
Zi-D-aN <ashrafzidan996@gmail.com>
2018-12-23 20:16:17 +0000

    hopefully relevant message about this commit

---
+++ src/app/firebase.config.ts
@@ -0,0 +1,8 @@
+export const FIREBASE_CONFIG = {
+    apiKey: "AIzaSyDnzR0e3mJfft02wSFVuKXvqajE_NZl110",
+   authDomain: "bank-helper-b59e4.firebaseapp.com",
+   databaseURL: "https://bank-helper-b59e4.firebaseio.com",
+   projectId: "bank-helper-b59e4",
+   storageBucket: "bank-helper-b59e4.appspot.com",
+   messagingSenderId: "306861240365"
+}
